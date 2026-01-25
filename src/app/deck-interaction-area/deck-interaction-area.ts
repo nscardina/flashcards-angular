763e921bcf63778b3c1ebaceb7f74c87134e1ad3
Noah Scardina <88603144+nscardina@users.noreply.github.com>
2026-01-25 14:33:49 -0500

    Added functionality to "forward" and "back" buttons to the side of the card view

--- src/app/deck-interaction-area/deck-interaction-area.ts
+++ src/app/deck-interaction-area/deck-interaction-area.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardDisplay } from '../card-display/card-display';
 import { MatButtonModule, MatMiniFabButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AppUIState } from '../app-uistate';
 
 @Component({
   selector: 'fc-deck-interaction-area',
@@ -14,5 +15,7 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './deck-interaction-area.scss',
 })
 export class DeckInteractionArea {
+
+  appState = inject(AppUIState)
 
 }
